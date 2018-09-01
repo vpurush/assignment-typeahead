3c0c83dabd0c6118eb9c24edaa5eaf6be751073a
vpurush <vijayaraghavan1987@gmai.com>
2018-09-01 11:27:33 +0530

    dom removal on scroll-out and dom addition on scroll-in  - feature implemented

--- src/public/product/product-result-item.component.ts
+++ src/public/product/product-result-item.component.ts
@@ -53,16 +53,41 @@ export class ProductResultItemComponent implements AfterViewInit{
             let isInView = this.isInViewport(this.elm.nativeElement);
             // console.log("isInView", isInView);
             if(isInView){
+                this.showMe();
                 this.scrolledIntoView.emit();
             }else{
-
+                this.hideMe();
             }
-        }, 500);
+        }, 250);
     }
 
     debouceTimer:any = null;
     ngAfterViewInit(){
-
         document.addEventListener('scroll', this.scrollHandler.bind(this));
+    }
+
+    hidden: boolean = false;
+    height: Number = null;
+    width: Number = null;
+    hideMe(){
+        if(!this.hidden){
+            this.hidden = true;
+
+            // There is some performance issue with the following code, hence i'm hard coding
+            // let padding = parseInt(getComputedStyle(this.elm.nativeElement).padding);
+            let padding = 16; //padding is 16px for 100% zoom on desktop
+            let border = 2;
+
+            this.height = this.elm.nativeElement.offsetHeight - (2*padding) - border;
+            this.width = this.elm.nativeElement.offsetWidth - (2*padding) - border;
+
+            console.log("height width", this.height, this.width);
+        }
+    }
+
+    showMe(){
+        this.hidden = false;
+        this.height = null;
+        this.width = null;
     }
 }
